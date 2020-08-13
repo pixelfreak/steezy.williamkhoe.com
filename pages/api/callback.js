5732d4987dbb0c557b74e381fb0fc85b8be1ee83
William Khoe <pixelfreak@gmail.com>
2020-08-13 18:27:58 -0500

    Update callback.js

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -3,11 +3,14 @@ import auth0 from '../../lib/auth0';
 export default async function Callback(req, res) 
 {
     let redirectTo = '/';
-    let prefix = req.query.state.indexOf('redirect=');
-    if (prefix >= 0)
+
+    const state = Buffer.from(req.query.state || '', 'base64').toString('ascii');
+    const prefix = 'redirect=';
+    const prefixIndex = state.indexOf(prefix);
+
+    if (prefixIndex >= 0)
     {
-        const state = Buffer.from(req.query.state || '', 'base64').toString('ascii');
-        redirectTo = state.slice(prefix, state.indexOf('|'))
+        redirectTo = state.slice(prefixIndex + prefix.length, state.indexOf('|'));
     }
     
     try
